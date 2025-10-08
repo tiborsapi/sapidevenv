800640c542607d2a0812cd3e0391532bb4c32a41
Tibor Kiss <kiss.tibor@ms.sapientia.ro>
2025-10-08 06:01:57 +0000

    Add docker instalation steps for Ubuntu 24

---
+++ install_docker_ubuntu.sh
@@ -0,0 +1,17 @@
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+docker --version
+docker compose version
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl status docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
+docker compose version
+docker buildx version
+
